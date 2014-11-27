274e4b37e7798d8004f010a1a0710f2d31e65280
bernamorillo <bernamorillo@gmail.com>
2014-11-27 14:26:03 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -149,8 +149,8 @@
 										width: 600,
 										height: 200,
 										renderer: 'line',
-										min: parseFloat(datastream.min_value) - .01*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
-										max: parseFloat(datastream.max_value) + .01*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+										min: 0,
+										max: 40,
 										padding: {
 											top: 0.02,
 											right: 0.02,

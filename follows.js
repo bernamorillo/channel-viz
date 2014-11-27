1740f7d3fa48c37cd72bb3d8b4ba3b81e5b80c04
bernamorillo <bernamorillo@gmail.com>
2014-11-27 12:37:05 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -13,11 +13,11 @@
 	*/
 
 	var defaultKey		= 'OMCzabPFgxwth38NaNLqwgpOPSfj0CIllfjzVynlhgj8FDeE', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [3368415 !V1-CO2], // Comma separated array of Xively Feed ID numbers
+		defaultFeeds	= [3368415], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'SANTA ENGRACIA XIVELY', // Replaces Xively logo in the header
 		dataDuration	= '1day', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
-		dataColor		= '72f92e', // CSS HEX value of color to represent data (omit leading #)
+		dataColor		= 'random', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
